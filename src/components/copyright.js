27ab74d86f6dfd8c4a68f338397d1608d8eba1b8
teetTeadusklubi <127295279+teetTeadusklubi@users.noreply.github.com>
2023-03-10 06:32:20 +0200

    Commit from TeleportHQ

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -7,53 +7,12 @@ const Copyright = (props) => {
     <div className="copyright-copyright">
       <div className="copyright-max-width max-content-container">
         <span className="copyright-text">
-          <span className="copyright-text1">
-            © All rights reserved
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
-          </span>
-          <a
-            href="https://www.teleporthq.io"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text2">@TeleportHQ.</span>
-          </a>
-          <span className="copyright-text3">
-            {' '}
-            Powered by
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
-          </span>
-          <a
-            href="https://www.vercel.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text4">Vercel</span>
-          </a>
-          <span className="copyright-text5">
-            . Image source:
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
-          </span>
-          <a
-            href="https://www.unsplash.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text6">Unsplash</span>
-          </a>
-          <span className="copyright-text7">.</span>
+          © All rights reserved
+          <span
+            dangerouslySetInnerHTML={{
+              __html: ' ',
+            }}
+          />
         </span>
       </div>
     </div>
