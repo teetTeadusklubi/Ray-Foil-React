6d73b158e3ad6021d45834b32a9a634a87dc163c
teetTeadusklubi <127295279+teetTeadusklubi@users.noreply.github.com>
2023-05-23 09:54:20 +0300

    Commit from TeleportHQ

--- src/views/our-story.js
+++ src/views/our-story.js
@@ -74,7 +74,7 @@ const OurStory = (props) => {
             <path d="M327.196 795.328c55.31-36.15 124.080-63.636 199.788-80.414-15.054-17.784-28.708-37.622-40.492-59.020-30.414-55.234-46.492-116.058-46.492-175.894 0-86.042 0-167.31 30.6-233.762 29.706-64.504 83.128-104.496 159.222-119.488-16.914-76.48-61.94-126.75-181.822-126.75-192 0-192 128.942-192 288 0 99.060 57.502 198.104 128 237.832v52.78c-217.102 17.748-384 124.42-384 253.388h279.006c14.518-12.91 30.596-25.172 48.19-36.672z"></path>
           </svg>
           <span className="our-story-text11">Customers</span>
-          <span className="our-story-text12">Lorem ipsum dolor sit amet.</span>
+          <span className="our-story-text12">Have tested our foils</span>
           <h1 className="our-story-text13">
             <span>50</span>
           </h1>
@@ -83,19 +83,17 @@ const OurStory = (props) => {
           <svg viewBox="0 0 1024 1024" className="our-story-icon3">
             <path d="M598 512h234l-234-234v234zM640 214l256 256v426q0 34-26 60t-60 26h-470q-34 0-59-26t-25-60v-598q0-34 26-59t60-25h298zM682 42v86h-512v598h-84v-598q0-34 25-60t59-26h512z"></path>
           </svg>
-          <span className="our-story-text15">Projects</span>
-          <span className="our-story-text16">Lorem ipsum dolor sit amet.</span>
-          <h1 className="our-story-text17">
-            <span>300</span>
-          </h1>
+          <span className="our-story-text15">Patents</span>
+          <span className="our-story-text16">Patents pending</span>
+          <h1 className="our-story-text17">3</h1>
         </div>
         <div className="our-story-stat2">
           <svg viewBox="0 0 1024 1024" className="our-story-icon5">
             <path d="M810 640v-86h-84v86h84zM810 810v-84h-84v84h84zM554 298v-84h-84v84h84zM554 470v-86h-84v86h84zM554 640v-86h-84v86h84zM554 810v-84h-84v84h84zM298 470v-86h-84v86h84zM298 640v-86h-84v86h84zM298 810v-84h-84v84h84zM640 470h256v426h-768v-598h256v-84l128-128 128 128v256z"></path>
           </svg>
-          <span className="our-story-text19">Cities</span>
-          <span className="our-story-text20">Lorem ipsum dolor sit amet.</span>
-          <h1 className="our-story-text21">
+          <span className="our-story-text18">Locations</span>
+          <span className="our-story-text19">Have our Rays been riden</span>
+          <h1 className="our-story-text20">
             <span>20</span>
           </h1>
         </div>
